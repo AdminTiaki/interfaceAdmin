d488d1032fd75db98d2df78fe63e3a01e0446e30
DavidFer64 <davidfer6415@gmail.com>
2023-08-31 10:16:19 +0200

    Ajout méthode

--- src/app/unitaire/unitaire.component.ts
+++ src/app/unitaire/unitaire.component.ts
@@ -11,4 +11,64 @@ export class UnitaireComponent {
     return value > 10;
   }
 
+  checkConditionUnitaireComponent2(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent3(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent4(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent5(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent6(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent7(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent8(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent9(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent10(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent11(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent12(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent13(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent14(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent15(value: number): boolean {
+    return value > 10;
+  }
+
+  checkConditionUnitaireComponent16(value: number): boolean {
+    return value > 10;
+  }
+
 }
